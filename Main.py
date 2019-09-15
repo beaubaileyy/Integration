40690977f204f6bedde130a2d86e5a4d36eab6b8
beaubailey <bebailey5895@eagle.fgcu.edu>
2019-09-15 16:18:56 -0400

    added intro and part of addition problem

--- Main.py
+++ Main.py
@@ -1,1 +1,12 @@
 #Beau Bailey
+#This program will be a useful resource that includes all of the programming exercises and code that I have worked on in module resources.
+print("Hello and welcome to my program!")
+print("Please Complete the following addition problem!" )
+print("Enter two numbers that add up to 10")
+firstNumber = input("Enter a number: ")
+secondNumber = input("Enter another number: ")
+num1 = int(firstNumber)
+num2 = int(secondNumber)
+sum = num1 + num2
+print("sum = ", sum)
+
