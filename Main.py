0739d53d318e41c4753e81916b7dee5ffc0e64c9
beaubailey <bebailey5895@eagle.fgcu.edu>
2019-11-21 13:06:28 -0500

    Improved format, code more readable

--- Main.py
+++ Main.py
@@ -1,80 +1,110 @@
-#Beau Bailey
-#This program will be a useful resource that includes all of the programming exercises and code that I have worked on in module resources.
+# Beau Bailey
+# This program will be a useful resource that includes all of the programming
+# Exercises and code that I have worked on in module resources.
+
+
+def getSmaller(get_smaller_num1, get_smaller_num2):
+    if get_smaller_num1 < get_smaller_num2:
+        smaller_num = get_smaller_num1
+    else:
+        smaller_num = get_smaller_num2
+    return smaller_num
+
+
+def smaller():
+    get_smaller_user_input1 = int(input("Enter a number: "))
+    get_smaller_user_input2 = int(input("Enter a second number: "))
+
+    smaller_num = getSmaller(get_smaller_user_input1, get_smaller_user_input2)
+    print("The smaller of the two numbers is", smaller_num)
 
 
 def main():
 
     print("Hello and welcome to my Integration Project program!")
     print("If you dont mind me asking, what's your name?")
-    userName = input("My name is: ")
-    continueProgram = True
-    while continueProgram:
-        print("Which of the following choices would you like to look at " + userName + '?')
+    user_name = input("My name is: ")
+    continue_program = True
+    while continue_program:
+        print("Which of the following choices would you like to look at " +
+              user_name + '?')
+        print("Please enter the whole number that corresponds to the choice "
+              "you would like to see.")
         print("1. A simple addition problem")
         print("2. A simple multiplication problem")
         print("3. More complex addition loop")
         print("4. Value returning function problem")
         print("5. Quit")
-        userChoice = int(input())
+        user_choice = int(input())
 
-        if userChoice == 1:
-            print("This part of the program will prompt you to complete a simple addition problem.")
-            print("Complete the following addition problem!" )
+
+# Within this portion of code if the user types the whole number 1 it
+# Prompts them for a couple numbers to complete an addition problem
+        if user_choice == 1:
+            print("This part of the program will prompt you to complete a "
+                  "simple addition problem.")
+            print("Complete the following addition problem!")
             print("Enter two numbers that add up to 10")
-            firstAddNumber = input("Enter a number: ")
-            secondAddNumber = input("Enter another number: ")
-            AddNum1 = int(firstAddNumber)
-            AddNum2 = int(secondAddNumber)
-            sum = AddNum1 + AddNum2
-            print("sum = ", sum)
-            if(sum == 10) : print("Correct! Nice job " + userName + '!')
-            else: print("Please try again!")
+            first_add_number = input("Enter a number: ")
+            second_add_number = input("Enter another number: ")
+            add_num1 = int(first_add_number)
+            add_num2 = int(second_add_number)
+            add_num_sum = add_num1 + add_num2
+            print("sum = ", add_num_sum)
+            if sum == 10:
+                print("Correct! Nice job " + user_name + '!')
+            else:
+                print("Please try again!")
 
-        elif userChoice == 2:
-            print("This part of the program will prompt you to complete a simple multiplication problem.")
+# Within this portion of code if the user types the whole number 2 it prompts
+# The user for a couple numbers to complete an multiplication problem
+        elif user_choice == 2:
+            print("This part of the program will prompt you to complete a "
+                  "simple multiplication problem.")
             print("Now lets try it.\nEnter two numbers that multiply to 10")
-            firstMultNumber = input("Enter a number: ")
-            secondMultNumber = input("Enter another number: ")
-            MultNum1 = int(firstMultNumber)
-            MultNum2 = int(secondMultNumber)
-            product = MultNum1 * MultNum2
-            if(product == 10) : print("Correct! Nice job " + userName + '!')
-            else: print("Please try again!")
+            first_mult_number = input("Enter a number: ")
+            second_mult_number = input("Enter another number: ")
+            mult_num1 = int(first_mult_number)
+            mult_num2 = int(second_mult_number)
+            mult_num_product = mult_num1 * mult_num2
+            if mult_num_product == 10:
+                print("Correct! Nice job " + user_name + '!')
+            else:
+                print("Please try again!")
 
-        elif userChoice == 3:
-            print("This part of the program will continually prompt you for positive numbers and find the sum of them.")
-            print("Once you would like to stop entering numbers, simply input '0' or a negative number.")
-            print("Then the program will print the sum of all the numbers you entered.")
-            AdditionTotal = 0
-            AdditionNumber = int(input("Enter first positive number:"))
+        elif user_choice == 3:
+            print("This part of the program will continually prompt you for "
+                  "positive numbers and find the sum of them.")
+            print("Once you would like to stop entering numbers, simply "
+                  "input '0' or a negative number.")
+            print("Then the program will print the sum of all the numbers "
+                  "you entered.")
+            addition_total = 0
+            addition_number = int(input("Enter first positive number:"))
 
-            while AdditionNumber > 0 or AdditionNumber < 0:
-                AdditionTotal = AdditionTotal + AdditionNumber
-                AdditionNumber = int(input("Enter next number: "))
-            print(AdditionTotal)
-        elif userChoice == 4:
-            print("The following part of the program will prompt you to enter 2 numbers.")
-            print("It will then tell you which of the 2 numbers has the smallest value.")
-            print("This function uses the return function to set the call value equal to what was returned.")
-            def getSmaller(getSmallerNum1, getSmallerNum2):
-                if getSmallerNum1 < getSmallerNum2:
-                    smaller = getSmallerNum1
-                else:
-                    smaller = getSmallerNum2
-                return smaller
+            while addition_number > 0 or addition_number < 0:
+                addition_total = addition_total + addition_number
+                addition_number = int(input("Enter next number: "))
+            print(addition_total)
 
-            getSmallerUserInput1 = int(input("Enter a number: "))
-            getSmallerUserInput2 = int(input("Enter a second number: "))
+        elif user_choice == 4:
+            print("The following part of the program will prompt you to "
+                  "enter 2 numbers.")
+            print("It will then tell you which of the 2 numbers has the "
+                  "smallest value.")
+            print("This function uses the return function to set the call "
+                  "value equal to what was returned.")
 
-            smallerNumber = getSmaller(getSmallerUserInput1, getSmallerUserInput2)
-            print("The Smaller of the two numbers is", smallerNumber)
+            smaller()
 
-        elif userChoice == 5:
+        elif user_choice == 5:
             print("Thank you for checking my program out!")
-            print("Have a good day", userName + '!')
+            print("Have a good day", user_name + '!')
             break
 
         else:
             print("That wasn't an option. Please try again!")
             continue
+
+
 main()
